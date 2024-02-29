7635b85204c410e161ae0acef29788b700cc0f1e
actuponit <113873813+actuponit@users.noreply.github.com>
2024-02-29 10:24:44 +0300

    Add solution for Combination Sum

---
+++ Camp 1/leetcode/combination-sum.py
@@ -0,0 +1,24 @@
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        self.target = target
+        def backtrack(path, s, idx):
+            if s == self.target:
+                sol.append(path.copy())
+                return
+            if s > self.target:
+                return
+            for i in range(idx, len(candidates)):
+                if s + candidates[i] > self.target:
+                    break
+                if s < self.target:
+                    path.append(candidates[i])
+                    backtrack(path, s+candidates[i], i)    
+                    path.pop()
+                    continue
+
+                backtrack(path, s, i)
+                path.pop()
+        candidates.sort()
+        sol = []
+        backtrack([], 0, 0)
+        return sol
